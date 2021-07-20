67bc5b7f80803299a9f315cd6505b751e66710db
ChipMcCallahan <thisischipmccallahan@gmail.com>
2021-07-19 18:39:13 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='CC1LevelsetProto',
+    name='cc1_levelset_proto',
     url='https://github.com/ChipMcCallahan/CC1LevelsetProto',
     author='Chip McCallahan',
     author_email='thisischipmccallahan@gmail.com',
     # Needed to actually package something
-    packages=['cc1_levelset_proto'],
+    packages=find_packages(),
     # Needed for dependencies
     install_requires=[],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
-    license='GNU General Public License v3.0',
+    license='LICENSE',
     description='Protos and generated python files for working with CC1 levelsets.',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
