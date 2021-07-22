5e38878e21cdd92bc2e155f916584049004cc242
ChipMcCallahan <thisischipmccallahan@gmail.com>
2021-07-22 16:41:30 -0700

    add Levels helper

--- cc1_levelset_proto/CC1Levels.py
+++ cc1_levelset_proto/CC1Levels.py
@@ -7,27 +7,33 @@ def add(level, pos, tcode):
 	if tcode in CC1TileCodes.MONSTERS and pos not in level.movement:
 		level.movement.append(pos)
 
-def remove(level, pos, tcode):
-	tspec = level[pos]
-	if CC1TileSpecs.remove(tspec, tcode):
-		if tcode in CC1TileCodes.MONSTERS and pos in level.movement:
-			del level.movement[level.movement.index(pos)]
-		elif tcode == CC1TileCode.TRAP:
-			for k, v in tuple(level.trap_controls.items()):
-				if v == pos:
-					del level.trap_controls[k]
-		elif tcode == CC1TileCode.TRAP_BUTTON:
-			for k in tuple(level.trap_controls.keys()):
-				if k == pos:
-					del level.trap_controls[k]
-		elif tcode == CC1TileCode.CLONER:
-			for k, v in tuple(level.clone_controls.items()):
-				if v == pos:
-					del level.clone_controls[k]
-		elif tcode == CC1TileCode.CLONE_BUTTON:
-			for k in tuple(level.clone_controls.keys()):
-				if k == pos:
-					del level.clone_controls[k]
+def remove(level, pos, tcodes):
+	if isinstance(tcodes, int):
+        tcodes = [tcodes]
+    tspec = level[pos]
+    removed = False
+    for tcode in tcodes:
+		if CC1TileSpecs.remove(tspec, tcode):
+			if tcode in CC1TileCodes.MONSTERS and pos in level.movement:
+				del level.movement[level.movement.index(pos)]
+			elif tcode == CC1TileCode.TRAP:
+				for k, v in tuple(level.trap_controls.items()):
+					if v == pos:
+						del level.trap_controls[k]
+			elif tcode == CC1TileCode.TRAP_BUTTON:
+				for k in tuple(level.trap_controls.keys()):
+					if k == pos:
+						del level.trap_controls[k]
+			elif tcode == CC1TileCode.CLONER:
+				for k, v in tuple(level.clone_controls.items()):
+					if v == pos:
+						del level.clone_controls[k]
+			elif tcode == CC1TileCode.CLONE_BUTTON:
+				for k in tuple(level.clone_controls.keys()):
+					if k == pos:
+						del level.clone_controls[k]
+			removed = True
+	return removed
 
 def is_valid(level):
 	for tspec in level.map.values():
