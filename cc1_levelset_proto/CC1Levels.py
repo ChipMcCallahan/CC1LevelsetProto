d7b376c17ef5e03c4a972bd98a5a581cde23e5f0
ChipMcCallahan <thisischipmccallahan@gmail.com>
2021-07-23 19:55:52 -0700

    if adding a tile at a pos removes a trap/cloner or button, remove the connection

--- cc1_levelset_proto/CC1Levels.py
+++ cc1_levelset_proto/CC1Levels.py
@@ -1,9 +1,11 @@
-from cc1_levelset_proto.cc1_levelset_pb2 import CC1TileCode
+from cc1_levelset_proto.cc1_levelset_pb2 import CC1TileCode, TileSpec
 from cc1_levelset_proto import CC1TileCodes, CC1TileSpecs
 
 
 def add(level, pos, tcode):
     tspec = level.map[pos]
+    oldspec = TileSpec()
+    oldspec.CopyFrom(tspec)
     was_monster = tspec.top in CC1TileCodes.MONSTERS
     CC1TileSpecs.add(tspec, tcode)
     is_monster = tspec.top in CC1TileCodes.MONSTERS
@@ -13,8 +15,11 @@ def add(level, pos, tcode):
     if is_monster and not was_monster and len(level.movement) < 127:
         level.movement.append(pos)
 
-    # TODO: adding an element that removes a clone or trap connection
-    # will not modify the clone or trap connections.
+    for code in (CC1TileCode.TRAP, CC1TileCode.TRAP_BUTTON, CC1TileCode.CLONER,
+                 CC1TileCode.CLONE_BUTTON):
+        if code in (oldspec.top, oldspec.bottom) and code not in (
+        tspec.top, tspec.bottom):
+            update_controls(level, pos, code)
 
 
 def remove(level, pos, tcodes, *, keep_cloned_mobs=False):
@@ -27,21 +32,26 @@ def remove(level, pos, tcodes, *, keep_cloned_mobs=False):
             removed = True
             if tcode in CC1TileCodes.MONSTERS and pos in level.movement:
                 level.movement.remove(pos)
-            elif tcode == CC1TileCode.TRAP:
-                for k, v in tuple(level.trap_controls.items()):
-                    if v == pos:
-                        level.trap_controls.pop(k, None)
-            elif tcode == CC1TileCode.TRAP_BUTTON:
-                level.trap_controls.pop(pos, None)
             elif tcode == CC1TileCode.CLONER:
-                for k, v in tuple(level.clone_controls.items()):
-                    if v == pos:
-                        level.clone_controls.pop(k, None)
                 if not keep_cloned_mobs and tspec.top in CC1TileCodes.ENTITIES:
                     CC1TileSpecs.remove(tspec, tspec.top)
-            elif tcode == CC1TileCode.CLONE_BUTTON:
-                level.clone_controls.pop(pos, None)
+            update_controls(level, pos, tcode)
     return removed
+
+
+def update_controls(level, pos, tcode):
+    if tcode == CC1TileCode.TRAP:
+        for k, v in tuple(level.trap_controls.items()):
+            if v == pos:
+                level.trap_controls.pop(k, None)
+    elif tcode == CC1TileCode.TRAP_BUTTON:
+        level.trap_controls.pop(pos, None)
+    elif tcode == CC1TileCode.CLONER:
+        for k, v in tuple(level.clone_controls.items()):
+            if v == pos:
+                level.clone_controls.pop(k, None)
+    elif tcode == CC1TileCode.CLONE_BUTTON:
+        level.clone_controls.pop(pos, None)
 
 
 def is_valid(level):
